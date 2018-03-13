68f9ad735688bd0797a4a1a57905c34bbd380c9a
Bokharouss <a.k.bokharouss@student.tue.nl>
2018-03-13 10:52:47 +0100

    added new test cases (passed)

--- test/PerceptronLearnerTest.java
+++ test/PerceptronLearnerTest.java
@@ -32,8 +32,8 @@ public class PerceptronLearnerTest {
         String expResult = "4 ----++++";
         
         //the actual result
-        boolean useBias=true;
-        int maxNumberOfIterations=1000;
+        boolean useBias = true;
+        int maxNumberOfIterations = 1000;
         String result = instance.execute(pos, neg, useBias, maxNumberOfIterations, q);
         
         //test whether both are equal
@@ -41,33 +41,65 @@ public class PerceptronLearnerTest {
     }
     
     @Test
-    public void testExecute1() { // a template for an additional test
-        System.out.println("execute1");
+    public void testExecute1() { // XOR test
+        System.out.println("XOR test");
         
         //create the positive list
         List<PVector> pos = PVector.listOf(new Integer[][]{
-            // e.g. {2,  1}, {2,  3}, {3, -1}, {3,  2}
+            {0,  1}, {1,  0}
         });
         
         //create the negative list
         List<PVector> neg = PVector.listOf(new Integer[][]{
-            // e.g. {0, -2}, {1,  0}, {2, -1}, {3, -3}
+            {0, 0}, {1,  1}
         });
         
         //create the queries list
         List<PVector> q = PVector.listOf(new Integer[][]{
-//                {0, -2}, {1,  0}, {2, -1}, {3, -3},
-//                {2,  1}, {2,  3}, {3, -1}, {3,  2},
+                {0,  1}, {1,  0}, {0, 0}, {1,  1}
         });
         
         PerceptronLearner instance = new PerceptronLearner();
         
         //the expected result
-        String expResult = "4 ----++++";
+        String expResult = "1000"; // should not converge
         
         //the actual result
-        boolean useBias=true;
-        int maxNumberOfIterations=1000;
+        boolean useBias = true;
+        int maxNumberOfIterations = 1000;
+        String result = instance.execute(pos, neg, useBias, maxNumberOfIterations, q);
+        
+        //test whether both are equal
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testExecute2() { // XOR test
+        System.out.println("AND test");
+        
+        //create the positive list
+        List<PVector> pos = PVector.listOf(new Integer[][]{
+            {1, 1}
+        });
+        
+        //create the negative list
+        List<PVector> neg = PVector.listOf(new Integer[][]{
+            {0, 0}, {0, 1}, {1, 0}
+        });
+        
+        //create the queries list
+        List<PVector> q = PVector.listOf(new Integer[][]{
+                {0,  1}, {1,  0}, {0, 0}, {1,  1}
+        });
+        
+        PerceptronLearner instance = new PerceptronLearner();
+        
+        //the expected result
+        String expResult = "5 ---+"; // should not converge
+        
+        //the actual result
+        boolean useBias = true;
+        int maxNumberOfIterations = 1000;
         String result = instance.execute(pos, neg, useBias, maxNumberOfIterations, q);
         
         //test whether both are equal
